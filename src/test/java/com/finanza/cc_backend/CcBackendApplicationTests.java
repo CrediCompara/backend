4e1cb19c3179090aa245e4b7aa71bb42dc748420
RaulC2803 <raul_casanova_28@hotmail.com>
2021-05-25 16:46:18 -0500

    project created

---
+++ src/test/java/com/finanza/cc_backend/CcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanza.cc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

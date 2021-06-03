21c64bae9fee01bfcde392fe9f572a9b84468277
Alexander Meléndez <xandermelendez.2001@gmail.com>
2021-06-02 20:29:17 -0500

    Finished

--- src/main/java/com/finanza/cc_backend/service/UserServiceImpl.java
+++ src/main/java/com/finanza/cc_backend/service/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.finanza.cc_backend.service;
 
+import com.finanza.cc_backend.domain.model.MortgageCredit;
 import com.finanza.cc_backend.domain.model.User;
+import com.finanza.cc_backend.domain.repository.MortgageCreditRepository;
 import com.finanza.cc_backend.domain.repository.UserRepository;
 import com.finanza.cc_backend.domain.service.UserService;
+import com.finanza.cc_backend.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +14,20 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private MortgageCreditRepository mortagageCreditRepository;
+
+
     @Override
     public User createUser(User user) {
         return userRepository.save(user);
     }
+
+    @Override
+    public MortgageCredit getMortgageCredit(Long id) {
+         return mortagageCreditRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User","Id",id));
+
+    }
+
+
 }
